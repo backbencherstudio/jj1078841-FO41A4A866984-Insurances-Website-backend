56eb5e3a2a9af3a8bba83e5c672cb1f23495c707
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2025-08-03 15:40:36 +0600

    fix

---
+++ prisma/migrations/20250803093950_update/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "claims" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "first_name" TEXT,
+ADD COLUMN     "last_name" TEXT,
+ADD COLUMN     "phone_number" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "zip_code" TEXT;
